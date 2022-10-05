95907223a1a83238b3358d0b3ef91108f860d728
Nicholas Homme <nickhomme@gmail.com>
2022-10-05 12:13:24 -0400

    init

---
+++ Mud.Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Mud;
+using Mud.Types;
+
+// var jclass = new FileInfo(args[0]);
+// Console.WriteLine("Hello, World!");
+
+var jvm = new Jvm();
+var sigParser = jvm.CallStatic<JavaObject>("sun.reflect.generics.parser.SignatureParser", "make");
+sigParser.Call<();
+
